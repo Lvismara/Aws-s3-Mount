29cd173d8cb379aec0fa2227a14f221fecae7199
Lvismara <leo.vismara@gmail.com>
2018-07-10 09:16:42 -0300

    rm sudo param

--- mountShell.sh
+++ mountShell.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+cd /
+sudo su -
 while :
 do
-	sudo s3fs -o allow_other,uid=1000,gid=1000,umask=027 prealta /devops/prealta_files/ -o passwd_file=/.passwd-s3fs &
+	s3fs -o allow_other,uid=1000,gid=1000,umask=027 prealta /devops/prealta_files/ -o passwd_file=/.passwd-s3fs &
 	sleep 5
-
 done
