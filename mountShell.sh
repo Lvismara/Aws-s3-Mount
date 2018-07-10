5a24d9406bb4cbb1d626b01bce160b7dc4b93fd2
Lvismara <leo.vismara@gmail.com>
2018-07-10 08:51:32 -0300

    correcao mount bg

--- mountShell.sh
+++ mountShell.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-RETURN_VAL=1
-while [ "$RETURN_VAL" != "0" ]
+while :
 do
-	sudo s3fs -o allow_other,uid=1000,gid=1000,umask=027 prealta /devops/prealta_files/ -o passwd_file=/.passwd-s3fs -o dbglevel=info -o curldbg
-	RETURN_VAL= $?
+	sudo s3fs -o allow_other,uid=1000,gid=1000,umask=027 prealta /devops/prealta_files/ -o passwd_file=/.passwd-s3fs &
 	sleep 5
+
 done
