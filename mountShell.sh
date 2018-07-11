0e701fed9371c0a3bb240b80ae4a5c5a1609214f
Lvismara <leo.vismara@gmail.com>
2018-07-11 13:23:07 -0300

    verificação while

--- mountShell.sh
+++ mountShell.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-cd /
-sudo su -
-while :
-do
+while [ $? -ne 0 ];do
 	s3fs -o allow_other,uid=1000,gid=1000,umask=027 prealta /devops/prealta_files/ -o passwd_file=/.passwd-s3fs &
-	sleep 5
+	sleep 2
 done
