6873b17429d8b9a469598cb73aeea377c3a0eb99
Lvismara <leo.vismara@gmail.com>
2018-07-11 15:24:46 -0300

    validação de end

--- mountShell.sh
+++ mountShell.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-while [ $? -ne 0 ];do
-	s3fs -o allow_other,uid=1000,gid=1000,umask=027 prealta /devops/prealta_files/ -o passwd_file=/.passwd-s3fs &
-	sleep 2
+RET=1
+until [ ${RET} -eq 0 ]; do
+	s3fs -o allow_other,uid=1000,gid=1000,umask=027 prealta /devops/prealta_files/ -o passwd_file=/.passwd-s3fs 
+	RET=$?
+    	sleep 2
 done
